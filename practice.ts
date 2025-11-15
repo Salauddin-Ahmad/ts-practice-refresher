ba5e44c8529ed6f022f981decd9e948f85a894db
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2025-11-15 08:20:20 +0600

    first commit

---
+++ practice.ts
@@ -0,0 +1,73 @@
+
+// const createArrayWithGeneric = <T>(value: T) => {
+//     return [value]
+// }
+
+// const arrString = createArrayWithGeneric<string>("Apple");
+// const arrNum = createArrayWithGeneric<number>(22);
+
+// const arrObj = createArrayWithGeneric<object>({
+//     id: 1,
+//     name: "next level"
+// })
+
+// const createArrayWithTuple = (param: string, param2: number) => 
+//     [param,
+//     param2,]
+
+
+//     const createArrayWithTupleGeneric = <X, Y>(param: X, param2: Y) => [param, param2]
+
+//     const res1 = createArrayWithTupleGeneric<string, number>('hh', 11)
+//     const res2 = createArrayWithTupleGeneric<string, number>('hh', 11)
+
+
+
+// GENERIC FUNCTION
+
+// const addCourseStudent = <T>(studentInfo: T ) => {
+//     return {
+//         course: "Next Level",
+//         ...studentInfo
+//     }
+// }
+
+//     const student1 ={
+//         id: 123,
+//         name: 'Mejba',
+//         haspen: true,
+//     }
+
+//     const student2 ={
+//         id: 321,
+//         name: 'Jhankar Mahabub',
+//         hasCar: true,
+//         isMarried: true
+//     }
+
+
+//     const resutl = addCourseStudent<typeof student1>(student1)
+
+//     console.log(resutl)
+
+
+
+// GENERIC FUNCTION IMPROVED VERSION
+
+const addCourseStudent = <T extends object>(studentInfo: T):
+T & { course: string } => {
+  return {
+    course: "Next Level",
+    ...studentInfo,
+  };
+};
+
+const student1 = {
+  id: 123,
+  name: "Mejba",
+  haspen: true,
+};
+
+const resutl = addCourseStudent<typeof student1>(student1)
+
+console.log(resutl);
